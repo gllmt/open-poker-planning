02c1030f98cb6de3671db81295f836fafaac0eeb
Pierre Guillemot <pierregllmt@gmail.com>
2025-12-22 12:03:12 +0100

    refactor(poker): reorganize GameArea layout for improved structure
    - Updated GameArea component to enhance layout by moving GameController inside a flex container.
    - Removed redundant GameController instance from the previous position to streamline rendering.

--- components/poker/game-area.tsx
+++ components/poker/game-area.tsx
@@ -20,7 +20,14 @@ export function GameArea({
 }) {
   return (
     <>
-      <div className="text-center flex justify-center">
+    <div className="flex flex-col gap-4">
+    <GameController
+          game={game}
+          players={players}
+          currentPlayerId={currentPlayerId}
+        />
+    </div>
+      <div className="text-center flex justify-center pb-4">
         <CardPicker
           game={game}
           players={players}
@@ -31,11 +38,6 @@ export function GameArea({
       </div>
       <div className="flex flex-col min-h-[60%] overflow-auto p-0.5 justify-center">
         <Players
-          game={game}
-          players={players}
-          currentPlayerId={currentPlayerId}
-        />
-        <GameController
           game={game}
           players={players}
           currentPlayerId={currentPlayerId}
