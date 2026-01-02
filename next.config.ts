f4c9d9370e9cbd68548c4b45613148712a53491f
Pierre Guillemot <pierregllmt@gmail.com>
2026-01-02 10:26:48 +0100

    chore(next-config): allow LAN origin in dev

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.1.20"],
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
